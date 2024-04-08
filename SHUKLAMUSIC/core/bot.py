8ffbf578b2e0a866ef09bb547e778426af723c70
Aditya <104716236+Latalove@users.noreply.github.com>
2024-04-08 12:22:44 +0530

    Update bot.py

--- SHUKLAMUSIC/core/bot.py
+++ SHUKLAMUSIC/core/bot.py
@@ -6,11 +6,11 @@ import config
 from ..logging import LOGGER
 
 
-class SHUKLA(Client):
+class PETER(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="SHUKLAMUSIC",
+            name="PETERMUSIC",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
